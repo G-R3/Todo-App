55d43b384457cc8d4d42ef2bf581dc2128b682df
Gerardo <gr261212@gmail.com>
2021-03-15 17:37:57 -0400

    Added Todo logic

---
+++ app.js
@@ -0,0 +1,39 @@
+const todoList = document.querySelector(".todo-list");
+const submitTodo = document.querySelector(".submit-todo-button");
+
+todoList.addEventListener("click", function (e) {
+  let target = e.target;
+  console.log(target);
+  if (e.target.classList.contains("trashcan")) {
+    console.log("deleted", target.parentNode);
+    // target.parentNode.classList.add("deleted");
+    target.parentNode.remove();
+  } else if (e.target.classList.contains("checkmark")) {
+    console.log("completed", target.parentNode);
+    target.parentNode.firstElementChild.classList.toggle("completed");
+  }
+});
+
+function newTodo(e) {
+  e.preventDefault();
+  let todo = document.querySelector(".todo-input");
+
+  let div = document.createElement("div");
+  let li = document.createElement("li");
+
+  let checkmarkBtn = document.createElement("button");
+  let trashcanBtn = document.createElement("button");
+
+  li.innerText = todo.value;
+  checkmarkBtn.classList.add("fas", "fa-check", "checkmark");
+  trashcanBtn.classList.add("fas", "fa-trash", "trashcan");
+  div.append(li);
+  div.append(checkmarkBtn);
+  div.append(trashcanBtn);
+  div.classList.add("todo-item");
+  todoList.append(div);
+
+  todo.value = "";
+}
+
+submitTodo.addEventListener("click", newTodo);
