e6042ac8f3698f1caa9fe05a338c93b590bd9298
Gerardo <gr261212@gmail.com>
2021-03-16 17:58:14 -0400

    Added localstorage

--- app.js
+++ app.js
@@ -14,6 +14,41 @@ todoList.addEventListener("click", function (e) {
   }
 });
 
+function saveTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function (todo) {
+    let div = document.createElement("div");
+    let li = document.createElement("li");
+    let checkmarkBtn = document.createElement("button");
+    let trashcanBtn = document.createElement("button");
+
+    li.innerText = todo;
+    checkmarkBtn.classList.add("fas", "fa-check", "checkmark");
+    trashcanBtn.classList.add("fas", "fa-trash", "trashcan");
+    div.append(li);
+    div.append(checkmarkBtn);
+    div.append(trashcanBtn);
+    div.classList.add("todo-item");
+    todoList.append(div);
+  });
+}
+
 function newTodo(e) {
   e.preventDefault();
   let todo = document.querySelector(".todo-input");
@@ -25,6 +60,7 @@ function newTodo(e) {
   let trashcanBtn = document.createElement("button");
 
   li.innerText = todo.value;
+  saveTodos(todo.value);
   checkmarkBtn.classList.add("fas", "fa-check", "checkmark");
   trashcanBtn.classList.add("fas", "fa-trash", "trashcan");
   div.append(li);
@@ -37,3 +73,4 @@ function newTodo(e) {
 }
 
 submitTodo.addEventListener("click", newTodo);
+window.addEventListener("load", loadTodos);
